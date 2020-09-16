18b23aa17026319aa8cef48264b0cd6a24983ddc
Jean Carlo <jeantoruno00@gmail.com>
2020-09-15 19:29:01 -0600

    Trabajo en clase 2

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
-function clickeado(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const id = href.substring(1);
-    console.log(id);
-    // Se oculta su totalidad, se muestra el elemento seleccionado
-    
-    for (let i = 0; i < contenido.length; i++) {
-        contenido[i].style.display = 'none';
-        if(contenido[i].getAttribute('id')== id) {
-            contenido[i].style.display = 'block';
-        }
-    }
-}
-
 const enlaces = document.querySelectorAll(' .tabs-tablist-item');
 
 const contenido = document.querySelectorAll(' .tabs-content-item');
 
 for (let i = 0; i < contenido.length; i++) {
-    contenido[i].style.display = 'none';
+    contenido[i].classList.add('js-content');
 }
+contenido[0].classList.remove('js-content');
+enlaces[0].classList.add('js-active');
 
-contenido[0].style.display = 'block';
-document.getElementById
 for (let i = 0; i < enlaces.length; i++) {
-    enlaces[i].addEventListener('click', clickeado);
+    enlaces[i].addEventListener('click', (event) => {
+        event.preventDefault();
+        const id = event.currentTarget.getAttribute('href').substring(1);
+        console.log(id);
+
+
+        for (let i = 0; i < contenido.length; i++) {
+            contenido[i].classList.add('js-content');
+            if (contenido[i].getAttribute('id') == id) {
+                contenido[i].classList.remove('js-content');
+            }
+        }
+        for (let i = 0; i < enlaces.length; i++) {
+            enlaces[i].classList.remove('js-active');
+        }
+        event.currentTarget.classList.add('js-active');
+    });
 }
